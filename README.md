2bbb82aca6ecc8a58d2f159025e18c4320aa6ded
Ian P <pritchardians@gmail.com>
2024-06-23 11:32:10 -0500

    add missing commits

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Wee Python Utilities
-Python utilities we find useful
+Python utilities we find useful <br>
 I suggest you copy the pytest tests as well as the code
+
+
+## Utilities
+- [Get Project Root Path](src/get_project_root.py)
---
+++ app.py
@@ -0,0 +1,7 @@
+from src.get_project_root import get_project_root
+
+# Run the utilities - use this as a how-to reference, or just see them work
+
+
+if __name__ == '__main__':
+    print(f"\nProject root: {get_project_root('wee-python-utilities')}")
